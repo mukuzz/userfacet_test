985f940e50a61cf161b09f758af742810bc2e89b
Mukul Singh <mukulsingh1248@gmail.com>
2022-07-29 14:01:13 +0530

    update

--- class_booker/models.py
+++ class_booker/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 class Booking(models.Model):
     date = models.DateField(default=timezone.now)
+    day = models.CharField(max_length=10)
     start_time = models.CharField(max_length=10)
     end_time = models.CharField(max_length=10)
     teacher_full_name = models.CharField(max_length=256)
